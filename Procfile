109e5aa7376a9dc8e7c45381a9b723ffbde9fc7e
chloekek <chloekek@use.startmail.com>
2024-04-26 15:34:11 +0200

    Remove all files except migrate.py

--- Procfile
+++
@@ -1,2 +0,0 @@
-web: python -m flask --debug run --port $PORT
-db:  PGHOST=$PWD/run/db_dev tools/run_postgres.py run/db_dev
--- migrations/0008-transaction-type-annihilate.sql
+++
@@ -1,7 +0,0 @@
--- migrate:up
-
-ALTER TYPE transaction_type ADD VALUE 'annihilate';
-
--- migrate:down
-
-ALTER TYPE transaction_type RENAME VALUE 'annihilate' TO '_deprecated_annihilate';
